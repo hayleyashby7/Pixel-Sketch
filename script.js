801d7a0f3862455dcc7fe42463a60920476da93e
Hayley Ashby <hayleyashby7@gmail.com>
2022-04-02 16:28:33 +0100

    Initial grid layout

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 "use strict";
+const MAX_DIMENSION = 600;
 
-createBoard(16, 16);
+createBoard(16);
 
-function createBoard(numRows, numCols) {
+function setMaxHeightWidth(element) {
+  element.style.maxHeight = `${MAX_DIMENSION}px`;
+  element.style.maxWidth = `${MAX_DIMENSION}px`;
+}
+
+function setSquareSide(square, numPerSide) {
+  let sideLength = MAX_DIMENSION / numPerSide;
+
+  square.style.height = `${sideLength}px`;
+  square.style.width = `${sideLength}px`;
+}
+
+function createBoard(squaresPerSide) {
   let gameGrid = document.getElementById("game-grid");
+  setMaxHeightWidth(gameGrid);
 
   // Add board divs by rows of columns
-  for (let row = 1; row <= numRows; row++) {
-    for (let col = 1; col <= numCols; col++) {
+  for (let row = 1; row <= squaresPerSide; row++) {
+    for (let col = 1; col <= squaresPerSide; col++) {
       let newSquare = document.createElement("div");
       newSquare.classList.add("gameSquare");
       newSquare.classList.add("Row-" + row);
       newSquare.classList.add("Col-" + col);
-      newSquare.textContent = row + " , " + col;
-
+      setSquareSide(newSquare, squaresPerSide);
       gameGrid.append(newSquare);
     }
   }
